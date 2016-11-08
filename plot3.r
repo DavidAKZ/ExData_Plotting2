db7bf9de37cc025d4d4671c88a822b1ccc5b015f
DavidAKZ <d_k@picknowl.com.au>
2016-11-08 16:51:44 +1030

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -1,15 +1,12 @@
 ## load ggplot2 library
 library('ggplot2')
 
-## re state x and y variables from plot 2 for Baltimore City
+## re state object from plot 2 for Baltimore City
 summarySCC_PM25_BaltimoreCity <- subset(summarySCC_PM25, summarySCC_PM25$fips==24510)
 
-x<-summarySCC_PM25_BaltimoreCity$year
-y<-summarySCC_PM25_BaltimoreCity$Emissions
-
-## plot emission v four polution source types
+## plot emissions per year v four polution source types
 png(file='plot3.png', width=480, height=480)
-qplot(x,y, data=summarySCC_PM25_BaltimoreCity, color=type, xlab='Year', ylab='mp2.5 Emissions', main='Emission Type-Baltimore City')
+qplot(year, Emissions, data=summarySCC_PM25_BaltimoreCity, facets =.~type) 
 dev.off
 
 
