9a2c8107d855eea9f3cdcbd530e9efe4682a8a24
DavidAKZ <d_k@picknowl.com.au>
2016-11-08 15:16:36 +1030

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,1 +1,8 @@
+x<- summarySCC_PM25$year
+y<- summarySCC_PM25$Emissions
 
+png(file='plot1.png', width=480, height=480)
+
+plot(x,y, pch=19, main="pm2.5 by Year", xlab="Year", ylab="pm2.5 Emissions")
+
+dev.off()
