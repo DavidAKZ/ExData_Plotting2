3bbaaf15e98be4f426884a40a55c6cb3d48c1b6b
DavidAKZ <d_k@picknowl.com.au>
2016-11-15 15:14:58 +1030

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -12,10 +12,10 @@ Source_Classification_Code <- na.omit(Source_Classification_Code)
 mergeDf <- merge(summarySCC_PM25, Source_Classification_Code, by='SCC', all=TRUE)
 
 ## Subset the resulting df for each coal related SCC valiue and plot accordingly
-## x= 30501023, 30531008, 30531009, 30531010, 30531011, 30531012, 30531014, 30531015, 30531016, 30531017, 30531090, 30531099
+## scc= 30501023, 30531008, 30531009, 30531010, 30531011, 30531012, 30531014, 30531015, 30531016, 30531017, 30531090, 30531099
 ## The following SCC values have been omitted due to lack of data points, 30531001, 30531004, 30531006, 30531007
 
-SCC_MergedDf <- subset(mergeDf, mergeDf$SCC=='x')  
+SCC_MergedDf <- subset(mergeDf, mergeDf$SCC=='scc')  
 png(file='plot3.png', width=480, height=480)
 qplot(Emissions, year, data=SCC_MergedDf, facets=.~Short.Name)
 dev.off
