d921c60171401c552755626186f33cdd04ada1b3
DavidAKZ <d_k@picknowl.com.au>
2016-11-17 12:42:16 +1030

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -6,16 +6,27 @@ library('ggplot2')
 summarySCC_PM25 <- na.omit(summarySCC_PM25)
 Source_Classification_Code <- na.omit(Source_Classification_Code)
 
+## Subset the Source Classification Code file for occurencies of 'coal'
+
+coal_SCC<- grep('coal', Source_Classification_Code$Short.Name, ignore.case = T)
+coal_subset_SCC <- Source_Classification_Code[coal_SCC,]
+
+## Join the coal reference file with the available pm2.5 data file
+coal_data <- merge(coal_subset_SCC,summarySCC_PM25)
+
+
+
+
+
+
+
 ## join both df objects to give a one-to-one correspondance between Source Classification Code (SCC) and Description to identify those
 ## SCC codes associated with coal
 
-mergeDf <- merge(summarySCC_PM25, Source_Classification_Code, by='SCC', all=TRUE)
+##mergeDf <- merge(summarySCC_PM25, Source_Classification_Code, by='SCC', all=TRUE)
 
-## Subset the resulting df for each coal related SCC valiue and plot accordingly
-## scc= 30501023, 30531008, 30531009, 30531010, 30531011, 30531012, 30531014, 30531015, 30531016, 30531017, 30531090, 30531099
-## The following SCC values have been omitted due to lack of data points, 30531001, 30531004, 30531006, 30531007
 
-SCC_MergedDf <- subset(mergeDf, mergeDf$SCC=='scc')  
-png(file='plot3.png', width=480, height=480)
-qplot(Emissions, year, data=SCC_MergedDf, facets=.~Short.Name)
+ 
+png(file='plot4.png', width=480, height=480)
+qplot(year, Emissions, data=coal_data, facets =.~Short.Name)
 dev.off
